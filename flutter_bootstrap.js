c1164753ed0c65f05bc4bec4179eddcd59fe9156
aadishsamir123 <71179256+aadishsamir123@users.noreply.github.com>
2026-04-15 22:44:07 +0800

    Built web
    Branch: main
    Commit: 2dd78829d5c4daf446db297072bf956e0b173efb
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1878270989" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3065781991" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
