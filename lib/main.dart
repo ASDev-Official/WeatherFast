5f9b050d2e4447cf8903755f56568da7d109eadd
aadishsamir123 <71179256+aadishsamir123@users.noreply.github.com>
2026-05-23 14:02:53 +0800

    apparently i need to import cupertino to get iOS-style transitions

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
